b09e09cb1a61d68f3fd186e1b8fa89f58cd41d63
Nactik <capiauxnathan@gmail.com>
2022-01-26 14:49:42 +0100

    Clean code

--- command_line.py
+++ command_line.py
@@ -9,10 +9,9 @@ from tkinter import E
 from acquireLease import acquireLease
 
 import bosdyn.client
-from bosdyn.client.util import (add_common_arguments, setup_logging)
 
 
-from webrtc2 import WebRTCCommands
+from run_webrtc import WebRTCSaveCommand
 from bosdyn.client import spot_cam
 from bosdyn.client.lease import LeaseClient, LeaseKeepAlive
 from bosdyn.client.robot_command import RobotCommandClient, RobotCommandBuilder, blocking_stand
@@ -22,30 +21,7 @@ from bosdyn.api.spot_cam import audio_pb2
 from connect import connect
 
 
-def register_all_commands(subparsers, command_dict):
-    COMMANDS = [
-        WebRTCCommands
-    ]
-
-    for register_command in COMMANDS:
-        register_command(subparsers, command_dict)
-
-
 def main(args=None):
-    """Command-line interface for interacting with Spot CAM"""
-    parser = argparse.ArgumentParser(prog='bosdyn.api.spot_cam', description=main.__doc__)
-    add_common_arguments(parser)
-
-    command_dict = {}  # command name to fn which takes parsed options
-    subparsers = parser.add_subparsers(title='commands', dest='command')
-    subparsers.required = True
-
-    register_all_commands(subparsers, command_dict)
-
-    options = parser.parse_args(args=args)
-
-    setup_logging(verbose=options.verbose)
-
     # Create robot object and authenticate.
     sdk = bosdyn.client.create_standard_sdk('Spot CAM Client')
     spot_cam.register_all_service_clients(sdk)
@@ -61,36 +37,34 @@ def main(args=None):
     robot_audio_client = robot.ensure_client(AudioClient.default_service_name)
 
 
-    #sound = audio_pb2.Sound(name='bark')
-    #with open("assets/dog-bark4.wav", 'rb') as fh:
-    #    data = fh.read()
+    sound = audio_pb2.Sound(name='bark')
+    with open("assets/dog-bark4.wav", 'rb') as fh:
+        data = fh.read()
     
-    #robot_audio_client.load_sound(sound, data)
+    robot_audio_client.load_sound(sound, data)
 
-    lease_client, lease = acquireLease(robot);
+    lease_client, lease = acquireLease(robot)
 
-    try :
-        with LeaseKeepAlive(lease_client, return_at_exit=True):
-            # Power on the robot and stand it up.
-            robot.time_sync.wait_for_sync()
-            robot.power_on()
-            blocking_stand(robot_command_client)
+    with LeaseKeepAlive(lease_client, return_at_exit=True):
+        # Power on the robot and stand it up.
+        robot.time_sync.wait_for_sync()
+        robot.power_on()
+        blocking_stand(robot_command_client)
 
-            try:
-                print("Run detection")
-                result = command_dict[options.command].run(robot, options)
-                if args is None and result:
-                    # Invoked as a CLI, so print result
-                    print(result)
+        try:
+            print("Run detection")
+            webrtc = WebRTCSaveCommand()
 
-                return result
-            finally:
-                # Send a Stop at the end, regardless of what happened.
-                robot_command_client.robot_command(RobotCommandBuilder.stop_command())
-                
-    finally :
-        print("coucou")
-       # lease_client.return_lease(lease)
+            result = webrtc.run(robot)
+            if args is None and result:
+                # Invoked as a CLI, so print result
+                print(result)
+
+            return result
+        finally:
+            # Send a Stop at the end, regardless of what happened.
+            robot_command_client.robot_command(RobotCommandBuilder.stop_command())
+
 
 if __name__ == '__main__':
     main()
--- goTo.py
+++ goTo.py
@@ -8,6 +8,7 @@
 
 from curses.ascii import NUL
 from glob import glob
+from string import ascii_uppercase
 import time
 from bosdyn.api.basic_command_pb2 import RobotCommandFeedbackStatus
 import bosdyn.client.util
@@ -16,7 +17,7 @@ from bosdyn.client.robot_command import RobotCommandClient, RobotCommandBuilder
 from bosdyn.client import math_helpers
 from bosdyn.client.frame_helpers import ODOM_FRAME_NAME, VISION_FRAME_NAME, BODY_FRAME_NAME, get_se2_a_tform_b
 import asyncio
-import webrtc2
+import run_webrtc
 
 cmd_id = None
 hasFinishedMoving = False
@@ -24,7 +25,7 @@ isDone = False
 
 def checkDoneMoving(result):
     global cmd_id
-    webrtc2.moving = True
+    run_webrtc.moving = True
     cmd_id = result.result()
 
     print(f"Command done, cmd_id : {cmd_id}")
@@ -41,7 +42,7 @@ def checkFeedBack(result):
             traj_feedback.body_movement_status == traj_feedback.BODY_STATUS_SETTLED):
         print("Arrived at the goal.")
 
-        webrtc2.moving = False
+        run_webrtc.moving = False
         hasFinishedMoving = True
 
 
@@ -52,6 +53,8 @@ async def goTo(robot,dx: float = 0,dy: float = 0, dyaw: float = 0,frame=ODOM_FRA
     # Setup clients for the robot state and robot command services.
 
     print(f"JE VAIS AU DEGREE : {dyaw}")
+
+    cmd_id = None
     
     robot_state_client = robot.ensure_client(RobotStateClient.default_service_name)
     robot_command_client = robot.ensure_client(RobotCommandClient.default_service_name)
@@ -80,37 +83,11 @@ async def goTo(robot,dx: float = 0,dy: float = 0, dyaw: float = 0,frame=ODOM_FRA
     while not cmd_id:
         await asyncio.sleep(0.1)
 
+    """
+    hasFinishedMoving = False
 
-## Not used for now
-async def relative_move(dx, dy, dyaw, frame_name, robot_command_client, robot_state_client, stairs=False):
-    transforms = robot_state_client.get_robot_state().kinematic_state.transforms_snapshot
-
-    # Build the transform for where we want the robot to be relative to where the body currently is.
-    body_tform_goal = math_helpers.SE2Pose(x=dx, y=dy, angle=dyaw)
-    # We do not want to command this goal in body frame because the body will move, thus shifting
-    # our goal. Instead, we transform this offset to get the goal position in the output frame
-    # (which will be either odom or vision).
-    out_tform_body = get_se2_a_tform_b(transforms, frame_name, BODY_FRAME_NAME)
-    out_tform_goal = out_tform_body * body_tform_goal
-
-    # Command the robot to go to the goal point in the specified frame. The command will stop at the
-    # new position.
-    robot_cmd = RobotCommandBuilder.synchro_se2_trajectory_point_command(
-        goal_x=out_tform_goal.x, goal_y=out_tform_goal.y, goal_heading=out_tform_goal.angle,
-        frame_name=frame_name, params=RobotCommandBuilder.mobility_params(stair_hint=stairs))
-    end_time = 10.0
-    cmd_id = robot_command_client.robot_command(lease=None, command=robot_cmd,
-                                                end_time_secs=time.time() + end_time)
-    # Wait until the robot has reached the goal.
-    while True:
-        feedback = robot_command_client.robot_command_feedback(cmd_id)
-        mobility_feedback = feedback.feedback.synchronized_feedback.mobility_command_feedback
-        if mobility_feedback.status != RobotCommandFeedbackStatus.STATUS_PROCESSING:
-            print("Failed to reach the goal")
-            return False
-        traj_feedback = mobility_feedback.se2_trajectory_feedback
-        if (traj_feedback.status == traj_feedback.STATUS_AT_GOAL and
-                traj_feedback.body_movement_status == traj_feedback.BODY_STATUS_SETTLED):
-            print("Arrived at the goal.")
-            return True
-        time.sleep(1)
+    while not hasFinishedMoving:
+        if cmd_id :
+            feedbackFuture = robot_command_client.robot_command_feedback_async(cmd_id)
+            feedbackFuture.add_done_callback(checkFeedBack)
+    """
